8c1760952e9a2b2123ec2dc91af8e6ab7c24c588
smola14 <dominik.smolinsky@icloud.com>
2021-12-16 21:07:54 +0100

    pridanie randomNumb

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const answer = document.querySelector("#answer");
 fetch("math.json")
     .then(response=>response.json())
     .then(data=>{
-        question.textContent=data[Math.floor(Math.random() * 3)].question;
+        randomNumb = Math.floor(Math.random() * 3);
+        question.textContent=data[randomNumb].question;
 
         answer.addEventListener("submit", function(e){
           e.preventDefault();
           const value = answer.querySelector('input[type="text"]').value;
-          if(value==data[0].answer){
+          if(value==data[randomNumb].answer){
             console.log("spravne");
           }else{
             console.log("nespravne")
