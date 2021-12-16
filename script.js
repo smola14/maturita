3eba6fdf1270bf9b6aa3702460c16f4fe402665a
smola14 <dominik.smolinsky@icloud.com>
2021-12-16 20:51:57 +0100

    uprava funkcie na submit

--- script.js
+++ script.js
@@ -8,17 +8,18 @@ fetch("math.json")
     .then(response=>response.json())
     .then(data=>{
         question.textContent=data[0].question;
+
+        answer.addEventListener("submit", function(e){
+          e.preventDefault();
+          const value = answer.querySelector('input[type="text"]').value;
+          if(value==data[0].answer){
+            console.log("spravne");
+          }else{
+            console.log("nespravne")
+          }
+        });
     })
 
-answer.addEventListener("submit", function(e){
-  e.preventDefault();
-  const value = answer.querySelector('input[type="text"]').value;
-  if(value==3){
-    console.log("spravne");
-  }else{
-    console.log("nespravne")
-  }
-});
 
 
 
