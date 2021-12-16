ec1be4b02eb7a6e0a6292e51e22894edfb4b2189
smola14 <dominik.smolinsky@icloud.com>
2021-12-16 20:36:23 +0100

    zmena question z # na .

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 'use strict';
 
-const question = document.querySelector("#question");
-const answer = document.querySelector("#answer");
+const question = document.querySelector(".question");
 
-
-function myFunction(p1, p2) {
-    return p1 * p2;   // The function returns the product of p1 and p2
-  }
 
 fetch("math.json")
     .then(response=>response.json())
     .then(data=>{
-        question.textContent=data[1].id+'. '+data[1].question;
+        question.textContent=data[0].question;
     })
 
 
