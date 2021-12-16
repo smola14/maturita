a4baf826dfb19c761c598c715c6e9ca6006149c3
smola14 <dominik.smolinsky@icloud.com>
2021-12-16 21:39:32 +0100

    uprava answer eventlisteneru

--- script.js
+++ script.js
@@ -4,23 +4,25 @@ const question = document.querySelector("#question");
 const answer = document.querySelector("#answer");
 const imgQ = document.querySelector("#imgQ");
 var randomNumb;
+var otazka;
 
 fetch("/otazky/math/math.json")
     .then(response=>response.json())
     .then(data=>{
-        randomNumb = Math.floor(Math.random() * 3);
+        // randomNumb = Math.floor(Math.random() * 3);
+        randomNumb = 4;
         otazka = data[randomNumb];
 
         question.textContent=otazka.question;
+        if(otazka.isPicture){
+          imgQ.src = "kokot";
+        }
 
         answer.addEventListener("submit", function(e){
           e.preventDefault();
           var value = answer.querySelector('input[type="text"]').value;
 
           value = value.replace(/,/g, '.');
-          if(otazka.isPicture){
-            imgQ.src = otazka.picture;
-          }
 
           if(value==otazka.answer){
             console.log("spravne");
