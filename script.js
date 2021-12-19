8a57aa0643379075a14e7625f9b1d327570ef546
smola14 <dominik.smolinsky@icloud.com>
2021-12-19 14:19:58 +0100

    canvas remove

--- script.js
+++ script.js
@@ -57,30 +57,7 @@ function showQuestion(otazka){
   h4.textContent = otazka.options;
 }
 
-window.addEventListener("load", ()=>{
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
-  
-  let painting = false;
 
-  function startPosition(){
-    painting = true;
-  }
-  function finishedPosition(){
-    painting = false;
-  }
-  function draw(e){
-    if(!painting) return;
-    ctx.lineWidth = 5;
-    ctx.lineCap = "round";
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-  }
-
-  canvas.addEventListener("mousedown", startPosition);
-  canvas.addEventListener("mouseup", finishedPosition);
-  canvas.addEventListener("mousemove", draw);
-});
 
 
 
