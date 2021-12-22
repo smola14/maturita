ee9749a399430b73297f9251f6101988d0ec84d6
smola14 <dominik.smolinsky@icloud.com>
2021-12-22 21:07:19 +0100

    uprava funkcie odpovedi

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ const submit = document.querySelector(".btn-submit");
 const year = document.querySelector("#year");
 const submitting = document.querySelector(".submitting");
 const options = document.querySelector("#options");
+const body = document.querySelector("body");
+const btn = document.querySelectorAll(".btn");
+const btnA = document.querySelector(".btn-A");
+const btnB = document.querySelector(".btn-B");
+const btnC = document.querySelector(".btn-C");
+const btnD = document.querySelector(".btn-D");
+const btnE = document.querySelector(".btn-E");
 
 var randomNumb;
 var otazka;
@@ -30,11 +37,39 @@ function showQuestion1_20(otazka){
   answer.innerHTML = "<input type='text'><button class='btn btn-submit'>submit</button>";
 }
 
+// answer.addEventListener("submit", function(e){
+//   e.preventDefault();
+//   var value = answer.querySelector('input[type="text"]').value;
+//   value = value.replace(/,/g, '.');
+//   (otazka.answer == value) ? body.style.backgroundColor = "green" : body.style.backgroundColor="red";
+// })
+
+btnA.addEventListener("click", function(){
+  btnA.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
+})
+
+btnB.addEventListener("click", function(){
+  btnB.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
+})
+
+btnC.addEventListener("click", function(){
+  btnC.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
+})
+
+btnD.addEventListener("click", function(){
+  btnD.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
+})
+
+btnE.addEventListener("click", function(){
+  btnW.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
+})
+
+
 function showQuestion21_30(otazka){
   question.textContent = otazka.question;
   (otazka.isPicture) ? img.src = otazka.picture : img.src = "";
   options.textContent = otazka.options;
-  answer.innerHTML = "<button class='btn'>A</button><br><button class='btn'>B</button><br><button class='btn'>C</button><br><button class='btn'>D</button><br><button class='btn'>E</button>";
+  answer.innerHTML = "<button class='btn btn-A'>A</button><br><button class='btn btn-B'>B</button><br><button class='btn btn-C'>C</button><br><button class='btn btn-D'>D</button><br><button class='btn btn-E'>E</button>";
 }
 
 
