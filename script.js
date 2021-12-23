6bb2af44e96b215a9b5f898b0a05329fc5c88838
smola14 <dominik.smolinsky@icloud.com>
2021-12-23 12:16:01 +0100

    upravovanie fukncie formulara - btn/submit

--- script.js
+++ script.js
@@ -19,57 +19,112 @@ const btnB = document.querySelector(".btn-B");
 const btnC = document.querySelector(".btn-C");
 const btnD = document.querySelector(".btn-D");
 const btnE = document.querySelector(".btn-E");
+const container = document.querySelector(".container");
 
 var randomNumb;
 var otazka;
 
 fetch("/otazky/math/math.json")
     .then(response=>response.json())
-    .then(data=>{
+    .then(data=>{      
       randomNumb = Math.floor(Math.random() * 30);
       otazka = data[randomNumb];
       (otazka.id<21) ? showQuestion1_20(otazka) : showQuestion21_30(otazka);
+    
+      reload.addEventListener("click", function(e){
+        e.preventDefault();
+        body.style.backgroundColor="white";
+        randomNumb = Math.floor(Math.random() * 30);
+        otazka = data[randomNumb];
+        if(otazka.id<21){
+          showQuestion1_20(otazka);
+        }else{
+          showQuestion21_30(otazka);
+        }
+      });
     });
 
 function showQuestion1_20(otazka){
-  question.textContent = otazka.question;
+  question.textContent = otazka.id + '. ' + otazka.question;
   (otazka.isPicture) ? img.src = otazka.picture : img.src = "";
   answer.innerHTML = "<input type='text'><button class='btn btn-submit'>submit</button>";
 }
-
-// answer.addEventListener("submit", function(e){
-//   e.preventDefault();
-//   var value = answer.querySelector('input[type="text"]').value;
-//   value = value.replace(/,/g, '.');
-//   (otazka.answer == value) ? body.style.backgroundColor = "green" : body.style.backgroundColor="red";
-// })
-
-btnA.addEventListener("click", function(){
-  btnA.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
-})
-
-btnB.addEventListener("click", function(){
-  btnB.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
-})
-
-btnC.addEventListener("click", function(){
-  btnC.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
-})
-
-btnD.addEventListener("click", function(){
-  btnD.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
-})
-
-btnE.addEventListener("click", function(){
-  btnW.textContent == otazka.answer ?  body.style.backgroundColor = "green" : body.style.backgroundColor="red";
-})
-
 
 function showQuestion21_30(otazka){
   question.textContent = otazka.question;
   (otazka.isPicture) ? img.src = otazka.picture : img.src = "";
   options.textContent = otazka.options;
-  answer.innerHTML = "<button class='btn btn-A'>A</button><br><button class='btn btn-B'>B</button><br><button class='btn btn-C'>C</button><br><button class='btn btn-D'>D</button><br><button class='btn btn-E'>E</button>";
+  answer.innerHTML = "<button class='btn btn-A mx-2'>A</button><button class='btn btn-B mx-2'>B</button><button class='btn btn-C mx-2'>C</button><button class='btn btn-D mx-2'>D</button><button class='btn btn-E mx-2'>E</button>";
+}
+
+answer.addEventListener("submit", function(e){
+  e.preventDefault();
+  if(e.submitter.textContent=="A" || e.submitter.textContent=="B" || e.submitter.textContent=="C" || e.submitter.textContent=="D" || e.submitter.textContent=="E"){
+    if(otazka.answer==e.submitter.textContent){
+      rightAnswer();
+    }else{
+      wrongAnswer();
+    }
+  }else{
+    var value = answer.querySelector('input[type="text"]').value;
+    value = value.replace(/,/g, '.');
+    if(otazka.answer==value){
+      rightAnswer();
+    }else{
+      wrongAnswer();
+    }
+  }
+})
+
+// answer.addEventListener("submit", function(e){
+//   e.preventDefault();
+//   if(e.submitter.textContent=="A"){
+//     if(otazka.answer=="A"){
+//       rightAnswer();
+//     }else{
+//       wrongAnswer();
+//     }
+//   }else if(e.submitter.textContent=="B"){
+//     if(otazka.answer=="B"){
+//       rightAnswer();
+//     }else{
+//       wrongAnswer();
+//     }
+//   }else if(e.submitter.textContent=="C"){
+//     if(otazka.answer=="C"){
+//       rightAnswer();
+//     }else{
+//       wrongAnswer();
+//     }
+//   }else if(e.submitter.textContent=="D"){
+//     if(otazka.answer=="D"){
+//       rightAnswer();
+//     }else{
+//       wrongAnswer();
+//     }
+//   }else if(e.submitter.textContent=="E"){
+//     if(otazka.answer=="E"){
+//       rightAnswer();
+//     }else{
+//       wrongAnswer();
+//     }
+//   }else{
+//     var value = answer.querySelector('input[type="text"]').value;
+//     value = value.replace(/,/g, '.');
+//     if(otazka.answer==value){
+//       rightAnswer();
+//     }else{
+//       wrongAnswer();
+//     }
+//   }
+// })
+
+function rightAnswer(){
+  body.style.backgroundColor="green";
+}
+
+function wrongAnswer(){
+  body.style.backgroundColor="red";
 }
 
 
