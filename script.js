cb74fa39a1d49f015769612321e89fdff09ca6ce
smola14 <dominik.smolinsky@icloud.com>
2021-12-26 23:12:41 +0100

    n

--- script.js
+++ script.js
@@ -83,7 +83,12 @@ function wrongAnswer(){
   body.style.backgroundColor="red";
 }
 
-
+const otazky = {
+  "id": 30,
+  "year": 2013,
+  "question": "When $a \ne 0$, there are two solutions to \(ax^2 + bx + c = 0\) and they are $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$",
+  "answer": 47.75
+}
 
 
 
