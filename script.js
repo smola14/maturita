c9ecd1ca7773c823ec69c645cf548e322da1d549
smola14 <dominik.smolinsky@icloud.com>
2021-12-28 21:27:59 +0100

    uprava cesty obrazkov + zmazanie starych

--- script.js
+++ script.js
@@ -25,255 +25,180 @@ const otazky = [
   {
       "id": 1,
       "year": 2013,
-      "question": "1. Päťnásobok neznámeho čísla zmenšený o \\(21\\) je práve toľko, koľko je dvojnásobok neznámeho čísla zväčšený o \\(15\\). Nájdite neznáme číslo.",
+      "picture" : "otazky/math/1.png",
       "answer": 12
   },
   {
       "id": 2,
       "year": 2013,
-      "question": "2. Nádrž tvaru kvádra má vnútorné rozmery vodorovného dna uvedené na obrázku. Hladina vody v nádrži siaha do výšky \\(980 cm\\). Koľko metrov kubických vody je v nádrži?",
-      "isPicture": true,
       "picture" : "otazky/math/2.png",
       "answer": 78.4
   },
   {
       "id": 3,
       "year": 2013,
-      "question": "3. Populácia mravcov vzrastie za jeden týždeň o \\(5%\\). Vypočítajte, o koľko percent vzrastie populácia mravcov takýmto tempom rastu za osem týždňov.",
+      "picture" : "otazky/math/3.png",
       "answer": 47.75
   },
   {
       "id": 4,
       "year": 2013,
-      "question": "4. Vypočítajte polomer kružnice k určenej rovnicou \\(x^2 + y^2 + 24x + 10y = 0\\)",
+      "picture" : "otazky/math/4.png",
       "answer": 13
   },
   {
       "id": 5,
       "year": 2013,
-      "question": "5. Na obrázku je znázornené vekové zloženie členov turistického krúžku. Zistite v rokoch medián veku členov turistického krúžku.",
-      "isPicture":true,
-      "picture": "otazky/math/5.png",
+      "picture" : "otazky/math/5.png",
       "answer": 47.75
   },
   {
       "id": 6,
       "year": 2013,
-      "question": "6. Dĺžky strán trojuholníka sú \\(3 cm\\), \\(4 cm\\) a \\(6 cm\\). Určte v stupňoch veľkosť tupého vnútorného uhla trojuholníka.",
-      "isPicture":true,
-      "picture": "otazky/math/6.png",
+      "picture" : "otazky/math/6.png",
       "answer": 117.28
   },
   {
       "id": 7,
       "year": 2013,
-      "question": "7. Presná hodnota čísla \\(17!\\) je \\(355 687 428 096 000\\). Hodnota čísla \\(17!\\) zobrazená po výpočte na kalkulačke je \\(3,556874281.10^14\\). Vypočítajte rozdiel hodnoty čísla \\(17!\\) zobrazenej na kalkulačke a presnej hodnoty čísla \\(17!\\).",
+      "picture" : "otazky/math/7.png",
       "answer": 4000
   },
   {
       "id": 8,
       "year": 2013,
-      "question": "8. V triede je \\(30\\) žiakov. Piati žiaci triedy mali na koncoročnom vysvedčení z matematiky trojku, ostatní žiaci triedy jednotku alebo dvojku. Priemer známok z matematiky všetkých žiakov triedy na koncoročnom vysvedčení bol \\(1,9\\). Zistite, koľko žiakov triedy malo na koncoročnom vysvedčení jednotku z matematiky.",
+      "picture" : "otazky/math/8.png",
       "answer": 8
   },
   {
       "id": 9,
       "year": 2013,
-      "question": "9. Graf lineárnej funkcie má smernicu \\(2\\), prechádza bodom A\\([2;8]\\) a súradnicovú os y pretína v bode B (pozrite obrázok). Určte vzdialenosť bodu B od začiatku súradnicovej sústavy O\\([0;0\\)].",
-      "isPicture":true,
-      "picture": "otazky/math/9.png",
+      "picture" : "otazky/math/9.png",
       "answer": 4
   },
   {
       "id": 10,
       "year": 2013,
-      "question": "10. Vypočítajte koreň rovnice \\(log x + log (x+3) = 1\\).",
+      "picture" : "otazky/math/10.png",
       "answer": 2
   },
   {
       "id": 11,
       "year": 2013,
-      "question": "11. Trojuholník ABC a trojuholník ADE sú podobné (pozrite obrázok). Vypočítajte v centimetroch štvorcových obsah trojuholníka ABC, ak dĺžka strany DE je \\(12 cm\\), dĺžka strany BC je \\(12 cm\\) a obsah trojuholníka ADE je \\(27 cm^2\\).",
-      "isPicture":true,
-      "picture": "otazky/math/11.png",
+      "picture" : "otazky/math/11.png",
       "answer": 48
   },
   {
       "id": 12,
       "year": 2013,
-      "question": "12. Dĺžky strán pravouhlého trojuholníka sú tri za sebou nasledujúce členy aritmetickej postupnosti. Dlhšia odvesna má dĺžku \\(24 cm\\). Vypočítajte v centimetroch dĺžku prepony trojuholníka.",
+      "picture" : "otazky/math/12.png",
       "answer": 47.75
   },
   {
       "id": 13,
       "year": 2013,
-      "question": "13. Dĺžka strany štvorca ABCD je \\(5 cm\\). Body E, F a G sú stredy strán štvorca (pozrite obrázok). Vypočítajte v centimetroch štvorcových obsah vyšrafovanej časti štvorca ABCD.",
+      "picture" : "otazky/math/13.png",
       "answer": 15.63,
-      "isPicture":true,
-      "picture": "otazky/math/13.png"
   },
   {
       "id": 14,
       "year": 2013,
-      "question": "14. Určte najmenšie celé číslo x, ktoré je riešením nerovnice \\(sqrt{17 - 15x - 2x^2}>\\) 0.",
+      "picture" : "otazky/math/14.png",
       "answer": -8
   },
   {
       "id": 15,
       "year": 2013,
-      "question": "15. V tabuľke je harmonogram sobotňajších tenisových tréningov mladších žiakov počas zimnej halovej sezóny. Pred začiatkom letnej sezóny sa pripravujenový harmonogram tréningov. Tomáš Kučera bude môcť trénovať len predpoludním,sestry Kováčové budú musieť trénovať v ľubovoľnom poradí za sebou. Ostatným žiakom vyhovujú všetky termíny. Koľko rôznych harmonogramov tenisových tréningov za uvedených podmienok je možné vytvoriť pre týchto osem žiakov?",
+      "picture" : "otazky/math/15.png",
       "answer": 3840,
-      "isPicture":true,
-      "picture": "otazky/math/15.png"
   },
   {
       "id": 16,
       "year": 2013,
-      "question": "16. Vo štvoruholníku ABCD s dĺžkami strán \\(|AB| = 9\\), \\(|BC| = 11\\) a \\(|CD| = 3\\) sú vnútorné uhly pri vrcholoch A a C pravé (pozrite obrázok). Určte dĺžku strany AD štvoruholníka ABCD.",
+      "picture" : "otazky/math/16.png",
       "answer": 7,
-      "isPicture":true,
-      "picture": "otazky/math/16.png"
   },
   {
       "id": 17,
       "year": 2013,
-      "question": "17. Ťažisko rovnostranného trojuholníka ABC leží v začiatku súradnicovej sústavy \\(O [0;0]\\) a vrchol C má súradnice \\([0;4]\\) . Vypočítajte súčet súradníc všetkých vrcholov trojuholníka ABC.",
+      "picture" : "otazky/math/17.png",
       "answer": 0
   },
   {
       "id": 18,
       "year": 2013,
-      "question": "18. Vandal vytrhol z knihy jeden list, na ktorom boli dve očíslované strany. Súčet čísel zvyšných strán knihy bol \\(7495\\) . Zistite, koľko očíslovaných strán mala pôvodne kniha, ak číslovanie strán knihy začalo číslom \\(1\\) .",
+      "picture" : "otazky/math/18.png",
       "answer": 123
   },
   {
       "id": 19,
       "year": 2013,
-      "question": "19. Obsah plášťa kužeľa je \\(4 cm^2\\), obsah podstavy kužeľa je \\(2 cm^2\\). Určte v stupňoch uhol(odchýlku) strany kužeľa a roviny podstavy kužeľa. (Strana kužeľa je úsečka spájajúca vrchol kužeľa s ľubovoľným bodom kružnice podstavy. Všetky strany kužeľa tvoria plášť kužeľa.)",
+      "picture" : "otazky/math/19.png",
       "answer": 60
   },
   {
       "id": 20,
       "year": 2013,
-      "question": "20. Určte najmenšie prirodzené číslo, ktorého súčin cifier je \\(240\\) .",
+      "picture" : "otazky/math/20.png",
       "answer": 568
   },
   {
       "id": 21,
       "year": 2013,
-      "question": "21. Určte počet koreňov rovnice \\(sin x = c/d\\) patriacich do intervalu \\((-570°; 570°)\\).",
-      "option1": 3,
-      "option2": 4,
-      "option3": 5,
-      "option4": 6,
-      "option5": 7,
+      "picture" : "otazky/math/21.png",
       "answer": "D"
   },
   {
       "id": 22,
       "year": 2013,
-      "question": "22. Obvod a obsah kruhu sú vyjadrené rovnakým číslom \\(x cm\\) a \\(x cm^2\\). Určte v centimetroch priemer kruhu.",
-      "option1": 4,
-      "option2": 2,
-      "option3": 1,
-      "option4": "π",
-      "option5": "4π",
+      "picture" : "otazky/math/22.png",
       "answer": "A"
   },
   {
       "id": 23,
       "year": 2013,
-      "question": "23. Simona má dva žreby, každý z inej lotérie. V prvej lotérii je \\(150 000\\) žrebov a z nich vyhráva \\(50 000\\)0, v druhej lotérii je \\(500 000\\) žrebov a z nich vyhráva \\(200 000\\) žrebov. Aká veľká je pravdepodobnosť, že vyhrá aspoň jeden Simonin žreb?",
-      "option1": "$3 * \\dfrac{40}{50}$",
-      "option2": "\\(dfrac{1}{Rb}\\)",
-      "option3": "\\(f(x) = x^2 + 2x - 14\\)",
-      "option4": "\\frac 2 3",
-      "option5": "\\frac 11 15",
+      "picture" : "otazky/math/23.png",
       "answer": "C"
   },
   {
       "id": 24,
       "year": 2013,
-      "question": "24. Určte súradnice bodov, v ktorých sa pretínajú grafy funkcií \\(f(x) = x^2 + 2x - 14\\) a \\(g(x) = x - 2\\). Najväčšia zo súradníc priesečníkov grafov funkcií je",
-      "option1": 2,
-      "option2": 3,
-      "option3": 4,
-      "option4": 5,
-      "option5": 6,
+      "picture" : "otazky/math/24.png",
       "answer": "B"
   },
   {
       "id": 25,
       "year": 2013,
-      "question": "25. Dĺžka hrany kocky ABCDEFGH je \\(4 cm\\). Vypočítajte povrch ihlana ABCDH.",
-      "option1": "64/3 cm2",
-      "option2": "32+16",
-      "option3": 4,
-      "option4": "96cm2",
-      "option5": "36cm2",
-      "answer": "B",
-      "isPicture":true,
-      "picture": "otazky/math/25.png"
+      "picture" : "otazky/math/25.png",
+      "answer": "B"
   },
   {
       "id": 26,
       "year": 2013,
-      "question": "26. Ak výrok B∧C je pravdivý a výrok B⇒A je nepravdivý, potom pre pravdivostnú hodnotu výrokov A, B, C platí:",
-      "option1": "Výrok A je pravdivý, výrok B je pravdivý, výrok C je pravdivý",
-      "option2": "Výrok A je pravdivý, výrok B je pravdivý, výrok C je nepravdivý",
-      "option3": "Výrok A je nepravdivý, výrok B je pravdivý, výrok C je pravdivý",
-      "option4": "Výrok A je pravdivý, výrok B je neavdivý, výrok C je pravdivý",
-      "option5": "Výrok A je nepravdivý, výrok B je nepravdivý, výrok C je pravdivý",
+      "picture" : "otazky/math/26.png",
       "answer": "C"
   },
   {
       "id": 27,
       "year": 2013,
-      "question": "27. Dané sú fukncie \\(f_1\\) až \\(f_6\\). Vyberte možnost, v ktorej sú z daných funkcii \\(f_1\\) až \\(f_6\\) uvedené len všetky funkcie rastúce na celom svojom definičnom obore.",
-      "option1": "\\(f_1\\), \\(f_6\\)",
-      "option2": "\\(f_2\\), \\(f_4\\), \\(f_5\\)",
-      "option3": "\\(f_2\\), \\(f_3\\), \\(f_4\\)",
-      "option4": "\\(f_3\\), \\(f_4\\), \\(f_5\\)",
-      "option5": "\\(f_4\\), \\(f_5\\)",
-      "isPicture" : true,
       "picture" : "otazky/math/27.png",
       "answer": "E"
   },
   {
       "id": 28,
       "year": 2013,
-      "question": "28. Šperk je vyrobený tak, že pravidelný osemsten zo zlata je zaliaty do kocky zo skla(pozrite obrázok). Určte pomer objemu skla a objemu zlata v šperku.(Pravidelný osemsten je teleso, ktoré vznikne zjednotením dvoch zhodných pravidelných ihlanov so spoločnou štvorcovou podstavou. Steny ihlanov sú rovnostranné trojuholníky.)",
-      "option1": "3:1",
-      "option2": "4:1",
-      "option3": "5:1",
-      "option4": "6:1",
-      "option5": "7:1",
-      "isPicture" : true,
       "picture" : "otazky/math/28.png",
       "answer": "C"
   },
   {
       "id": 29,
       "year": 2013,
-      "question": "29. Určte počet celých čísel, ktoré vyhovujú nerovnici \\(|x - 4| < 2π\\).",
-      "option1": 9,
-      "option2": 10,
-      "option3": 11,
-      "option4": 12,
-      "option5": 13,
+      "picture" : "otazky/math/29.png",
       "answer": "E"
   },
   {
       "id": 30,
       "year": 2013,
-      "question": "30. Ktorá kocka mohla byť zložená z následujúcej siete?",
-      "isOptPicture" : true,
-      "option1": "otazky/math/30oA.png",
-      "option2": "otazky/math/30oB.png",
-      "option3": "otazky/math/30oC.png",
-      "option4": "otazky/math/30oD.png",
-      "option5": "otazky/math/30oE.png",
-      "isPicture" : true,
       "picture" : "otazky/math/30.png",
       "answer": "B"
   }
