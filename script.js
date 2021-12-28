2a26cdc00101ffa598ac6263b1d5f133e2e8150c
smola14 <dominik.smolinsky@icloud.com>
2021-12-28 23:22:00 +0100

    uprava funkcionality na obrazky

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 'use strict';
 
-const question = document.querySelector("#question");
+
 const answer = document.querySelector("#answer");
 const mainIMG = document.querySelector("#mainIMG");
 const input = document.querySelector("input");
-const kontakt = document.querySelector(".kontakt");
 const main = document.querySelector(".main");
-const contactForm = document.querySelector("#form-contact");
 const reload = document.querySelector(".btn-reload");
 const submit = document.querySelector(".btn-submit");
-const year = document.querySelector("#year");
 const submitting = document.querySelector(".submitting");
-const options = document.querySelector("#options");
 const body = document.querySelector("body");
 const btn = document.querySelectorAll(".btn");
-const btnA = document.querySelector(".btn-A");
-const btnB = document.querySelector(".btn-B");
-const btnC = document.querySelector(".btn-C");
-const btnD = document.querySelector(".btn-D");
-const btnE = document.querySelector(".btn-E");
 const container = document.querySelector(".container");
+const userNumber = document.querySelector(".userNumber");
 
 const otazky = [
   {
@@ -212,33 +204,21 @@ let opts;
 function loadQuestion() {
     body.style.backgroundColor="white";
     randomNumb = Math.floor(Math.random() * 30);
-    otazka = otazky[29];
+    otazka = otazky[randomNumb];
     mainIMG.src = otazka.picture;
     if(otazka.id<21){
-          
+        answer.innerHTML="<input type='text'><button class='btn btn-submit'>Submit</button>";      
     }else{
-        
+        answer.innerHTML="<button class='btn btn-A mx-2'>A</button><button class='btn btn-B mx-2'>B</button><button class='btn btn-C mx-2'>C</button><button class='btn btn-D mx-2'>D</button><button class='btn btn-E mx-2'>E</button>"; 
     }
-}
-
-function showQuestion1_20(otazka){  
-    (otazka.isPicture) ? img.src = otazka.picture : img.src = "";
-    answer.innerHTML = "<div class='d-block'><input type='text' class='d-inline mx-1 rounded'><button class='btn btn-submit d-inline mx-1'>SUBMIT</button></div>";
-}
-
-function showQuestion21_30(otazka){
-    (otazka.isOptPicture) ? innerOptions(true) : innerOptions(false);
-    (otazka.isPicture) ? img.src = otazka.picture : img.src = "";
 }
 
 answer.addEventListener("submit", function(e){
     e.preventDefault();
     if(e.submitter.textContent=="A" || e.submitter.textContent=="B" || e.submitter.textContent=="C" || e.submitter.textContent=="D" || e.submitter.textContent=="E"){
         if(otazka.answer==e.submitter.textContent){
-            (otazka.isOptPicture) ? innerOptions(true) : innerOptions(false);
             buttonAnswerC(e.submitter.textContent,"lightgreen");
         }else{
-            (otazka.isOptPicture) ? innerOptions(true) : innerOptions(false);
             buttonAnswerC(e.submitter.textContent,"red");
         }
     }else{
@@ -252,23 +232,19 @@ answer.addEventListener("submit", function(e){
     }
 })
 
-function innerOptions(odpo){
-    if(odpo){
-        answer.innerHTML = "<div class='d-block'><button class='btn btn-A m-2'>A</button><img src="+otazka.option1+" class='img-fluid'></div><div class='d-block'><button class='btn btn-B m-2'>B</button><img src="+otazka.option2+" class='img-fluid'></div><div class='d-block'><button class='btn btn-C m-2'>C</button><img src="+otazka.option3+" class='img-fluid'></div><div class='d-block'><button class='btn btn-D m-2'>D</button><img src="+otazka.option4+" class='img-fluid'></div><div class='d-block'><button class='btn btn-E m-2'>E</button><img src="+otazka.option5+" class='img-fluid'><div>";
-    }else{
-        answer.innerHTML = "<div class='d-block my-3'><button class='btn btn-A mx-3'>A</button><span id='opt-1'>"+ otazka.option1 +"</span></div><div class='d-block my-3'><button class='btn btn-B mx-3'>B</button><span id='opt-2'>"+ otazka.option2 +"</span></div><div class='d-block my-3'><button class='btn btn-C mx-3'>C</button><span id='opt-3'>"+ otazka.option3 +"</span></div><div class='d-block my-3'><button class='btn btn-D mx-3'>D</button><span id='opt-4'>"+ otazka.option4 +"</span></div><div class='d-block my-3'><button class='btn btn-E mx-3'>E</button><span id='opt-5'>"+ otazka.option5+"</span><div>";
-        document.querySelector("#opt-1").textContent = otazka.option1;
-        renderMathInElement(document.querySelector("#opt-1"), opts);
-        document.querySelector("#opt-2").textContent = otazka.option2;
-        renderMathInElement(document.querySelector("#opt-2"), opts); 
-        document.querySelector("#opt-3").textContent = otazka.option3;
-        renderMathInElement(document.querySelector("#opt-3"), opts); 
-        document.querySelector("#opt-4").textContent = otazka.option4;
-        renderMathInElement(document.querySelector("#opt-4"), opts); 
-        document.querySelector("#opt-5").textContent = otazka.option5;
-        renderMathInElement(document.querySelector("#opt-5"), opts); 
+userNumber.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        otazka = otazky[(userNumber.value)-1];
+        mainIMG.src = otazka.picture;
+        if(otazka.id<21){
+            answer.innerHTML="<input type='text'><button class='btn btn-submit'>Submit</button>";      
+        }else{
+            answer.innerHTML="<button class='btn btn-A mx-2'>A</button><button class='btn btn-B mx-2'>B</button><button class='btn btn-C mx-2'>C</button><button class='btn btn-D mx-2'>D</button><button class='btn btn-E mx-2'>E</button>"; 
+        }
+        userNumber.placeholder=userNumber.value;
+        userNumber.value="";
     }
-}
+});
 
 function buttonAnswerC(pismenko,farba){
     var buttonColor = ".btn-"+pismenko;
